d6745c0cae3b1d4f4df8f80f118b4efb292f3e31
Jan Lebert <jan.lebert@online.de>
2015-05-19 11:29:47 +0200

    Support all SUL-Wikimedia projects
    Query watchlist for all projects where the user has made at least one
    edit.
    
    Bug: T99021
    Change-Id: Ie1ea88d1df84d519bc2672423d7fd54c2d13f18a

--- backend/celery/tasks.py
+++ backend/celery/tasks.py
@@ -4,20 +4,85 @@
 # Copyright (C) 2015 Jan Lebert
 from __future__ import absolute_import
 
+from contextlib import closing
+import MySQLdb
+
+from .. import config
 from . import app, logger
 from .api import MediaWiki
 
 
+def chunks(l, n):
+    """
+    Yield successive n-sized chunks from l.
+
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
 @app.task
 def initial_task(obj):
+    """
+    Task called on login, start the chain to load the
+    watchlist, notifications etc.
+    :param obj: dict with redis channel and oauth keys
+
+    """
     mw = MediaWiki(access_token=obj['access_token'])
+    username = mw.get_username()
     wikis = mw.get_wikis()
-    projects = ['enwiki', 'dewiki', 'itwiki', 'frwiki', 'itwiki',
-                'commonswiki', 'wikidatawiki', 'enwiktionary', 'dewiktionary',
-                'metawiki', 'mediawikiwiki']
-    for project in projects:
-        obj['wiki'] = wikis[project]
-        watchlistgetter.delay(obj)
+
+    db = MySQLdb.connect(
+        host='centralauth.labsdb',
+        user=config.sql_user,
+        passwd=config.sql_passwd
+    )
+
+    projects = []
+    with closing(db.cursor()) as cur:
+        cur.execute("SELECT lu_wiki FROM centralauth_p.localuser WHERE lu_name=%s;", [username])  # NOQA
+        result = cur.fetchall()
+        for row in result:
+            project = row[0]
+            try:
+                wiki = wikis[project]
+                if 'closed' not in wiki:
+                    projects.append(wiki)
+            except KeyError:
+                logger.error("Could not find %s in list of wikis" % project)
+    db.close()
+
+    for chunk in chunks(projects, 50):
+        check_editcount.delay(obj, chunk, username)
+
+
+@app.task
+def check_editcount(obj, project_chunk, username):
+    """
+    Starts the watchlist- and notificationgetter if the
+    user has made more than one edit to a project
+    :param obj: The dict from tornado
+    :param project_chunk: list of projets
+    :param username:
+
+    """
+    db = MySQLdb.connect(
+        host='s4.labsdb',
+        user=config.sql_user,
+        passwd=config.sql_passwd
+    )
+    with closing(db.cursor()) as cur:
+        for wiki in project_chunk:
+            project = wiki['dbname']
+            cur.execute("SELECT user_editcount FROM `%s_p`.user  WHERE user_name='%s';" % (project, username))  # NOQA
+            result = cur.fetchone()
+
+            if result and int(result[0]) >= 1:
+                obj['wiki'] = wiki
+                watchlistgetter.delay(obj)
+
+    db.close()
 
 
 def fix_urls(html, url):
